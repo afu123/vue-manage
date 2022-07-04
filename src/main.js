08b86555f984c8d03b43b2d65090a2ce938c71d5
zhaojian <2885013032@qq.com>
2022-07-04 08:58:12 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: zhaojian
+ * @Date: 2022-07-04 08:48:55
+ * @LastEditors: zhaojian
+ * @LastEditTime: 2022-07-04 08:57:25
+ * @Description: 
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
@@ -6,6 +13,8 @@ import './assets/less/index.less'
 
 import router from '../router'
 import store from '../store/index'
+
+Vue.prototype.$store = store
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
